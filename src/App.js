f9f9224174edda0009a567787d3115d3c65692e7
Jasmyn Hicks <jasmynh44@gmail.com>
2022-05-18 06:22:50 -0600

    Add welcome message

--- src/App.js
+++ src/App.js
@@ -22,9 +22,14 @@ function App() {
 
   return (
     <>
-      {console.log('prompts and responses array in app', promptsAndResponses)}
+      {/* {console.log('prompts and responses array in app', promptsAndResponses)} */}
       <h1>AI Assitant</h1>
-      <p>Welcome Message</p>
+      <p>Hello! I'm your AI assistant. You can ask me anything and I'll do my best to help. The more specific and descriptive you are, the better I can serve you. Here are some examples of things you can request:</p>
+      <ul>
+        <li>How can I be the best intern?</li>
+        <li>How do I get Shopify to hire me?</li>
+        <li>How do I fix bugs in my code?</li>
+      </ul>
       <Form storeUserPrompt={storeUserPrompt} />
       <CardContainer data={promptsAndResponses} />
     </>
