6c379c6f8b03df7185e38ad5398368a13f964606
Jasmyn Hicks <jasmynh44@gmail.com>
2022-05-17 20:33:54 -0600

    Add card container

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import './App.css';
 import { Route } from 'react-router-dom';
 import Form from './Components/Form'
 import { getResponse } from './api-calls';
+import CardContainer from './Components/CardContainer';
 
 function App() {
   const [userPrompt, setUserPrompt] = useState('')
   const [response, setResponse] = useState('')
+  const [promptsAndResponses, setPromptsAndResponses] = useState([])
 
 
   const storeUserPrompt = (prompt) => {
     setUserPrompt(prompt.event)
     getResponse(prompt.event)
       .then(data => setResponse(data))
+      .then(setPromptsAndResponses(...promptsAndResponses, { prompt: userPrompt, response: response }))
   }
 
   return (
@@ -21,6 +24,7 @@ function App() {
       <h1>AI Assitant</h1>
       <p>Welcome Message</p>
       <Form storeUserPrompt={storeUserPrompt} />
+      <CardContainer data={promptsAndResponses} />
     </>
   );
 }
---
+++ src/Components/CardContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CardContainer = ({ data }) => {
+
+  const cards = data.map(card => {
+    <Card>
+      key=
+    </Card>
+  })
+}
+
+export default CardContainer
