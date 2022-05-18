46dbf3dbc2523ca549a1d2df7c84644537d2e666
Jasmyn Hicks <jasmynh44@gmail.com>
2022-05-17 18:21:55 -0600

    Add form component

---
+++ src/Components/Form.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Form = () => {
+  return (
+    <form>
+      <input type='text' id='userPrompt' name='user_prompt' value='type any request'></input>
+      <button type='submit'>Submit</button>
+    </form>
+
+  )
+}
+
+
+export default Form 
