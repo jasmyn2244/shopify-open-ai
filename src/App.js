90afe79b564ecf7022fc38e76bc869ae7da264ba
Jasmyn Hicks <jasmynh44@gmail.com>
2022-05-17 19:00:24 -0600

    Add event listeners to form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 import Form from './Components/Form'
 
 function App() {
+  const [userPromt, setUserPrompt] = useState('')
+  const [aiResponse, setAiResponse] = useState('')
+
+  // useEffect(() => {
+  //   getAiResponse(userPrompt)
+  // }, userPrompt)
+
+  const storeUserPrompt = (prompt) => {
+    setUserPrompt(prompt)
+  }
   return (
     <>
       <h1>AI Assitant</h1>
       <p>Welcome Message</p>
-      <Form />
+      <Form storeUserPrompt={storeUserPrompt} />
     </>
   );
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Form = () => {
+const Form = ({ storeUserPrompt }) => {
+  const [userPrompt, setUserPromt] = useState('')
+
+  const handleChange = (event) => {
+    setUserPromt(userPrompt => ({
+      ...userPrompt, event
+    }))
+  }
+
+  const submitPrompt = (event) => {
+    event.preventDefault()
+    storeUserPrompt(userPrompt)
+  }
+
   return (
     <form>
-      <input type='text' id='userPrompt' name='user_prompt' value='type any request'></input>
-      <button type='submit'>Submit</button>
-    </form>
+      <input type='text' id='userPrompt' name='user_prompt' placeholder='type any request' onChange={event => handleChange(event)}></input>
+      <button type='submit' onClick={submitPrompt}>Submit</button>
+    </form >
 
   )
 }
